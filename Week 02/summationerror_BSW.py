f93a00dd2120e265aa9023556e722bbec4070872
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-09-04 10:53:05 -0400

    Create summationerror_BSW.py

---
+++ Week 02/summationerror_BSW.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+N = int(input("Pick a number"))
+
+n, a, sum1, m, b, sum2 = 1, 0, 0, N, 0, 0
+
+
+while (n<=N):
+    a = 1/n
+    sum1+=a
+    n = n+1
+    
+while (m>=1):
+    b = 1/m
+    sum2+=b
+    m = m-1
+    
+ploteq = (sum1 - sum2)/(np.absolute(sum1)+np.absolute(sum2))
+print(N, ploteq)
+
