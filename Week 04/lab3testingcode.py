fdb82efc2553e618ef635336e249c8c934d6231e
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-09-11 10:56:00 -0400

    Create lab3testingcode.py

---
+++ Week 04/lab3testingcode.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 10:05:42 2019
+
+@author: BSWOOD9321
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.random as rand
+
+a = rand.randint(-1,1)
+print(a)
