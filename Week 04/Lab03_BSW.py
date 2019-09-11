7b6554bf658d8930afbb4046b3bf0be9ff1b6d36
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-09-11 10:55:53 -0400

    Create Lab03_BSW.py

---
+++ Week 04/Lab03_BSW.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 19:50:39 2019
+
+@author: Brandon
+"""
+
+import numpy as np 
+import numpy.random as rand
+import matplotlib.pyplot as plt
+
+list = [[0,0],[1,0]]
+
+x = 1
+y = 2
+
+list.append([x,y])
+
+[x,y] in list
+
