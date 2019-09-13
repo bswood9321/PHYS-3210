a053be62f8582fa5ce89851573a6880313675f96
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-09-13 10:50:33 -0400

    Update Lab03_BSW.py

--- Week 04/Lab03_BSW.py
+++ Week 04/Lab03_BSW.py
@@ -9,42 +9,61 @@ import numpy as np
 import numpy.random as rand
 import matplotlib.pyplot as plt
 
-Coordinates, X, Y, xtoss, ytoss, x, y = [],[],[],[],[],0,0
+Polarity,Coordinates, X, Y, xtoss, ytoss, x, y = [],[],[],[],[],[],0,0
 
- while not [x, y] in Coordinates:
-    Coordinates.append([x,y])
-    X.append(x)
-    Y.append(y)
-    xtoss.append(x)
-    ytoss.append(y)
-    R=rand.randint(0,1)
-    if R==1:
-        x = x+(rand.randint(-1,2))
-        if x ==X[-1]:
-            y = y+(rand.randint(-1,2))
-            while y==Y[-1]:
-                y=y+(rand.randint(-1,2))
-    else:            
-        y=y+(rand.randint(-1,2))
-        if y==Y[-1]:
-            x=x+(rand.randint(-1,2))
-            while x==X[-1]:
-                x=x+(rand.randint(-1,2))
-    while [x,y] in Coordinates:
-                  
-            x=xtoss[-1]
-            y=ytoss[-1]
-            S=rand.randint(0,1)
-            if S==1:
-                x=x+(rand.randint(-1,2))
-                if x==xtoss[-1]:
-                    y=y+(rand.randint(-1,2))
-                    while y==ytoss[-1]:
-                        y=y+(rand.randint(-1,2))
-            else:            
-                y=y+(rand.randint(-1,2))
-                if y==ytoss[-1]:
-                    x=x+(rand.randint(-1,2))
-                    while x==xtoss[-1]:
-                        x=x+(rand.randint(-1,2))
-
+while not [x, y] in Coordinates:
+   Coordinates.append([x,y])
+   X.append(x)
+   Y.append(y)
+   xtoss.append(x)
+   ytoss.append(y)
+   P=rand.randint(-1,2)
+   if P == -1:
+       Polarity.append(1)
+   else: 
+       Polarity.append(0)
+   R=rand.randint(0,1)
+   if R==1:
+       x = x+(rand.randint(-1,2))
+       if x ==X[-1]:
+           y = y+(rand.randint(-1,2))
+           while y==Y[-1]:
+               y=y+(rand.randint(-1,2))
+   else:            
+       y=y+(rand.randint(-1,2))
+       if y==Y[-1]:
+           x=x+(rand.randint(-1,2))
+           while x==X[-1]:
+               x=x+(rand.randint(-1,2))
+   while [x,y] in Coordinates:
+       if ([X[-1]+1,Y[-1]] in Coordinates) \
+        and ([X[-1]-1,Y[-1]] in Coordinates) \
+        and ([X[-1],Y[-1]+1] in Coordinates) \
+        and ([X[-1],Y[-1]-1] in Coordinates):
+           break
+           
+       else:
+                 
+           x=xtoss[-1]
+           y=ytoss[-1]
+           S=rand.randint(0,1)
+           if S==1:
+               x=x+(rand.randint(-1,2))
+               if x==xtoss[-1]:
+                   y=y+(rand.randint(-1,2))
+                   while y==ytoss[-1]:
+                       y=y+(rand.randint(-1,2))
+           else:            
+               y=y+(rand.randint(-1,2))
+               if y==ytoss[-1]:
+                   x=x+(rand.randint(-1,2))
+                   while x==xtoss[-1]:
+                       x=x+(rand.randint(-1,2))
+                 
+L=str(len(Coordinates)-1)
+plt.plot(X,Y)
+plt.title("Self-Avoiding Random Walker")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+print("The length of this protein chain is: ",L)                       
