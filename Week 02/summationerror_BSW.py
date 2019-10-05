160e9edb64a82eed4268e607973f83f9506771f4
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-10-05 16:57:27 -0400

    Update summationerror_BSW.py

--- Week 02/summationerror_BSW.py
+++ Week 02/summationerror_BSW.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 
 
-N = int(input("Pick a number"))
+N = int(input("Pick a number: "))
 
 n, a, sum1, m, b, sum2 = 1, 0, 0, N, 0, 0
 
@@ -24,6 +24,37 @@ while (m>=1):
     sum2+=b
     m = m-1
     
-ploteq = (sum1 - sum2)/(np.absolute(sum1)+np.absolute(sum2))
-print(N, ploteq)
+error = (sum1 - sum2)/(np.absolute(sum1)+np.absolute(sum2))
+print("For N = :",N,", the error is: ",error)
 
+N1=1
+Ns=[]
+Errors=[]
+
+while N1<=N:
+
+    n1, a1, sum11, m1, b1, sum21 = 1, 0, 0, N1, 0, 0
+
+
+    while (n1<=N1):
+        a1 = 1/n1
+        sum11+=a1
+        n1 = n1+1
+    while (m1>=1):
+        b1 = 1/m1
+        sum21+=b1
+        m1 = m1-1
+
+    Ns.append(N1)
+    error1 = (sum11 - sum21)/(np.absolute(sum11)+np.absolute(sum21))
+    Errors.append(error1)
+    N1=N1+1
+    
+plt.plot(Ns,Errors,'r-')
+plt.xlabel("N")
+plt.ylabel("Error(x10^-16)")
+plt.title("N vs. Error")
+plt.show()    
+    
+print("The down summation is more precise because the error of the number is proportional to the number you are summing, therefore as the number decreases, there is less error, versus starting with a small number, and adding more error for every summation.")
+    
