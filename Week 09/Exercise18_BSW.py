f1a0478a7f958122fe8393be57480613f2f34ae6
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-10-18 10:55:12 -0400

    Update Exercise18_BSW.py

--- Week 09/Exercise18_BSW.py
+++ Week 09/Exercise18_BSW.py
@@ -12,14 +12,25 @@ import scipy.optimize as op
 data = np.loadtxt("pi_meson_decays.dat")
 time = data[:,0]
 decays=data[:,1]
-errors=np.sqrt(decays)
+errors=np.sqrt(decays)+2
 data=np.column_stack((data,errors))
 
 plt.errorbar(time,decays,yerr=errors)
 plt.title("Pi meson decays")
 plt.show()
 
-a1=430
-a2=2e-7    #guesses
-g=a1+a2*time
-plt.plot(time,g)
+
+
+
+def f(t,N0,T):
+    return N0*np.exp(-t/T)
+
+y = f(time,10000,2.6e-8)
+
+popt,pcov=op.curve_fit(f,time,decays)
+
+plt.plot(time,f(time,*popt))
+plt.errorbar(time,decays,yerr=errors)
+plt.xlabel('time')
+plt.ylabel('decays')
+plt.show()
