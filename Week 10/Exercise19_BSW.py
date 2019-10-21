f596a2b3862006230d8391833b878fa887f95d10
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-10-21 10:53:06 -0400

    Create Exercise19_BSW.py

---
+++ Week 10/Exercise19_BSW.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 21 10:01:29 2019
+
+@author: BSWOOD9321
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+m=1
+k=50
+p=2
+t=np.linspace(0,20,100)
+
+i=0
+
+
+    
