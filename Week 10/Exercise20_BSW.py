8c0c8f07617a1bdef43c4469454bf8bdda4c2195
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-10-25 10:55:34 -0400

    Update Exercise20_BSW.py

--- Week 10/Exercise20_BSW.py
+++ Week 10/Exercise20_BSW.py
@@ -8,12 +8,12 @@ Created on Wed Oct 23 09:59:34 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-k=1
-m=1
+k=5
+m=5
 dt=.01
 t=0
 x=0
-v=-10
+v=1
 p=2
 T=[t]
 X=[x]
@@ -23,14 +23,16 @@ while t<=15:
     f=-k*x**(p-1)
     v=v+(f/m)*dt
     kx=(v+(f/m)*dt/2)*dt
-    x=x+kx
-    kv=-(k/m)*(x+v*(dt/2))*dt   ##needs work on all this
+    kv=-(k/m)*(x+v*(dt/2))*dt   
     v=v+kv
+    x=x+kx
     T.append(t)
     X.append(x)
     V.append(v)
     t=t+dt
 plt.plot(T,X,label='position')
 plt.plot(T,V,label='velo')
+plt.legend(loc=(1.04,0))
+plt.grid()
 plt.show()    
     
