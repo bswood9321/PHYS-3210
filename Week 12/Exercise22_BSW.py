64accbd0d3f007e728a8bccc22673869f8ec058a
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-11-11 10:34:00 -0500

    Update Exercise22_BSW.py

--- Week 12/Exercise22_BSW.py
+++ Week 12/Exercise22_BSW.py
@@ -10,32 +10,56 @@ import matplotlib.pyplot as plt
 
 
 q=.0483
-E=15
+E=-10
 K=q*E
 V0=-83
 x=-100
 dx=1e-3
 X=[x]
-PSI=[np.exp(-K*np.abs(x))]
-dpsi=-(np.sign(x))*K*np.exp(-K*np.abs(x))
-psi=np.exp(-K*np.abs(x))
+PSI=[np.exp(K*np.abs(x))]
+dpsi=-(np.sign(x))*K*np.exp(K*np.abs(x))
+psi=np.exp(K*np.abs(x))
 a=2
 
-while x <=100:
+while x <=a:
     if np.abs(x)>a:
         V=0
     else:
         V=V0
         
-    d2psi=(q*V+K**2)*np.exp(-K*np.abs(x))
+    d2psi=(q*V+K**2)*np.exp(K*np.abs(x))
     dpsi=dpsi+d2psi*dx
-    psi=dpsi+dpsi*dx
+    psi=psi+dpsi*dx
     
     PSI.append(psi)
     x=x+dx
     X.append(x)
     
+x1=100
+X1=[x1] 
+PSI1=[np.exp(K*np.abs(x))]
+dpsi1=-(np.sign(x))*K*np.exp(K*np.abs(x))
+psi1=np.exp(K*np.abs(x))   
+
+while x1 >=a:
+    if np.abs(x1)>a:
+        V=0
+    else:
+        V=V0
+        
+    d2psi1=(q*V+K**2)*np.exp(K*np.abs(x1))
+    dpsi1=dpsi1+d2psi1*dx
+    psi1=psi1+dpsi1*dx
+    
+    PSI1.append(psi1)
+    x1=x1-dx
+    X1.append(x1)   
+    
+    
 plt.plot(X,PSI)
+plt.plot(X1,PSI1)
+plt.xlim(-5,5)
+
 plt.show()
     
     
