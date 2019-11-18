33e79531d24b99a4246492330b201d3a5128e245
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-11-18 10:51:19 -0500

    Create projectpractice_BSW.py

---
+++ New folder/projectpractice_BSW.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 10:26:02 2019
+
+@author: BSWOOD9321
+"""
+import numpy as np
+import mayavi3.mlab
+
+x,y,z=np.ogrid[-10:10:20j,-10:10:20j,-10:10,20j]
+s = np.sin(x*y*z)/(x*y*z)
+
+mlab.pipeline.volume(mlab.pipeline.scalar_field(s))
+mlab.show()
