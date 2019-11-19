de527ef0550790c68d948e5b3c3e5336441bd4b1
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-11-18 22:35:24 -0500

    Update projectpractice_BSW.py

--- New folder/projectpractice_BSW.py
+++ New folder/projectpractice_BSW.py
@@ -5,7 +5,7 @@ Created on Mon Nov 18 10:26:02 2019
 @author: BSWOOD9321
 """
 import numpy as np
-import mayavi3.mlab
+import mayavi.mlab
 
 x,y,z=np.ogrid[-10:10:20j,-10:10:20j,-10:10,20j]
 s = np.sin(x*y*z)/(x*y*z)
