80461e036cc3a6d13fe9effebded3495f377d4b2
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-11-30 18:34:02 -0500

    Update project_BSW.py

--- New folder/project_BSW.py
+++ New folder/project_BSW.py
@@ -7,7 +7,6 @@ Created on Mon Nov 11 22:17:26 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as pat
 from mpl_toolkits.mplot3d import Axes3D 
 
 
@@ -22,7 +21,7 @@ h=6.6260693e-34
 
 def r(n):
     rn=(4*np.pi*e0*(hbar**2)*n**2)/(me*(echarge**2))
-    return rn
+    return rn/a0
 
 def v(n):
     vn=hbar/(me*a0*n)
@@ -30,7 +29,7 @@ def v(n):
 
 def p(n):
     pn=hbar/(a0*n)
-    return pn
+    return pn/1e-24
 
 def E(n):
     En=-((me*(echarge**4))/(32*(np.pi**2)*(e0**2)*(hbar**2)))*(1/(n**2))
@@ -61,6 +60,8 @@ def Ze(z,n):
 def delZe(z,m,n):
     delZe=np.absolute(Ze(z,m)-Ze(z,n))
     return delZe
+
+
 
 def prob_1s(x,y,z):
     r=np.sqrt(np.square(x)+np.square(y)+np.square(z))
@@ -198,7 +199,7 @@ def prob_3p(x,y,z):
     r=np.sqrt(np.square(x)+np.square(y)+np.square(z))
     phi=np.arctan(y/x)
     theta=np.arccos(z/r)
-    return np.square((np.sqrt(2)/81)*(1/(np.sqrt(np.pi)))*(np.e**(-r/3))*(6-r)*np.cos(theta))
+    return np.square((np.sqrt(2)/81)*(1/(np.sqrt(np.pi)))*(np.e**(-r/3))*(6-r))
 
 x=np.linspace(-25,25,30)
 y=np.linspace(-25,25,30)
@@ -263,12 +264,18 @@ plt.show()
 
 n=1
 Es=[]
+radii=[]
 Vs=[]
+Ps=[]
 while n<=10:
     e=E(n)
+    rs=r(n)
+    vs=v(n)
+    ps=p(n)
+    radii.append([rs,rs])
     Es.append([e,e])
-    vel=v(n)
-    Vs.append([vel,vel])
+    Vs.append([vs,vs])
+    Ps.append([ps,ps])
     n=n+1
 
 
@@ -292,3 +299,113 @@ plt.title('Energy Levels of a Hydrogen Atom electron')
 plt.show()
 
 
+plt.figure(figsize=(1.5,6))
+plt.plot(x,radii[0],'k')
+plt.plot(x,radii[1],'k')
+plt.plot(x,radii[2],'k')
+plt.plot(x,radii[3],'k')
+plt.plot(x,radii[4],'k')
+plt.plot(x,radii[5],'k')
+plt.plot(x,radii[6],'k')
+plt.plot(x,radii[7],'k')
+plt.plot(x,radii[8],'k')
+plt.plot(x,radii[9],'k')
+plt.xlim(1.1,1.9)
+plt.yticks([1,4,8,16,25,36,49,64,81,100])
+plt.xticks([])
+plt.title('Quantized Radii of electron [Bohr radii]')
+plt.show()
+
+plt.figure(figsize=(1.5,6))
+plt.plot(x,Vs[0],'k')
+plt.plot(x,Vs[1],'k')
+plt.plot(x,Vs[2],'k')
+plt.plot(x,Vs[3],'k')
+plt.plot(x,Vs[4],'k')
+plt.plot(x,Vs[5],'k')
+plt.plot(x,Vs[6],'k')
+plt.plot(x,Vs[7],'k')
+plt.plot(x,Vs[8],'k')
+plt.plot(x,Vs[9],'k')
+plt.xlim(1.1,1.9)
+plt.yticks([2.2e6,1.1e6,7.3e5,5.5e5,4.4e5,3.6e5],('2.2e6','1.1e6','7.3e5','5.5e5','4.4e5','n=6+'))
+plt.xticks([])
+plt.title('Quantized Velocity of electron [m/s]')
+plt.show()
+
+plt.figure(figsize=(1.5,6))
+plt.plot(x,Ps[0],'k')
+plt.plot(x,Ps[1],'k')
+plt.plot(x,Ps[2],'k')
+plt.plot(x,Ps[3],'k')
+plt.plot(x,Ps[4],'k')
+plt.plot(x,Ps[5],'k')
+plt.plot(x,Ps[6],'k')
+plt.plot(x,Ps[7],'k')
+plt.plot(x,Ps[8],'k')
+plt.plot(x,Ps[9],'k')
+plt.xlim(1.1,1.9)
+plt.yticks([2,1,.66,.5,.4,.33],('2','1','.66','.5','.4','n=6+'))
+plt.xticks([])
+plt.title('Quantized momenta of electron [e-24 kg.m/s]')
+plt.show()
+
+zs=2
+
+ZEs=[]
+while zs<=5:
+    zes=Ze(zs,1)
+    ZEs.append([zes,zes])
+    zs=zs+1
+    
+plt.figure(figsize=(1.5,6))
+plt.plot(x,ZEs[0])
+plt.plot(x,ZEs[1])
+plt.plot(x,ZEs[2])
+plt.plot(x,ZEs[3])
+plt.xlim(1.1,1.9)
+plt.yticks([-54.43,-122.46,-217.71,-340.17],('He(-54.43Ev)','Li(-122.46eV)','Be(-217.71eV)','B(-340.17eV)'))
+plt.xticks([])
+plt.title('Ionizing Energy of ground state electron for Z=2-5')
+plt.show() 
+
+
+m,n=10,2
+
+BalmerphotonE=[]
+Balmerphotonwavelength=[]
+
+while m>n:
+    BPE=delE(m,n)
+    BPW=photonwavel(BPE)
+    BPWref=BPW/1e-9
+    BalmerphotonE.append(BPE)
+    Balmerphotonwavelength.append([BPWref,BPWref])
+    m=m-1
+    
+plt.figure(figsize=(15,1.5))
+ax=plt.axes()
+ax.set_facecolor('black')
+plt.plot(Balmerphotonwavelength[0],x, color='indigo')
+plt.plot(Balmerphotonwavelength[1],x, color='indigo')
+plt.plot(Balmerphotonwavelength[2],x, color='indigo')
+plt.plot(Balmerphotonwavelength[3],x, color='indigo')
+plt.plot(Balmerphotonwavelength[4],x, color='purple')
+plt.plot(Balmerphotonwavelength[5],x, color='blue')
+plt.plot(Balmerphotonwavelength[6],x, color='green')
+plt.plot(Balmerphotonwavelength[7],x, color='red')
+plt.ylim(1.1,1.9)
+plt.xticks([396,410,434,485,656],('<---UV','410nm','434nm','486nm','656nm'))
+plt.title('Balmer series Hydrogen Emission Spectrum')
+plt.yticks([])
+plt.show()
+
+
+    
+    
+    
+    
+    
+    
+    
+    
