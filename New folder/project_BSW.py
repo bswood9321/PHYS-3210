0125bf5f28f67bb907fe5fa280ace170b42fbd56
bswood9321 <54251053+bswood9321@users.noreply.github.com>
2019-12-05 22:19:42 -0500

    Update project_BSW.py

--- New folder/project_BSW.py
+++ New folder/project_BSW.py
@@ -404,7 +404,276 @@ plt.show()
     
     
     
+from mpl_toolkits.mplot3d import Axes3D 
+
+
+
+n=1
+L=1
+k=n*np.pi/L
+x=0
+psi=np.sqrt(2/L)*np.sin(k*x)
+dpsi=np.sqrt(2)*k*np.cos(k*x)/np.sqrt(L)
+X=[x]
+PSI=[0]
+P=[psi**2]
+dx=1e-5
+while x<=L:
+    d2psi=-k**2*(np.sqrt(2/L))*np.sin(k*x)
+    dpsi=dpsi+d2psi*dx
+    psi=psi+dpsi*dx
+    x=x+dx
+    X.append(x)
+    PSI.append(psi)
+    P.append((np.sqrt(2/L)*np.sin(k*x))**2)
+
+
+n=2
+L=1
+k=n*np.pi/L
+x=0
+psi=np.sqrt(2/L)*np.sin(k*x)
+dpsi=np.sqrt(2)*k*np.cos(k*x)/np.sqrt(L)
+PSI2=[0]
+P2=[psi**2]
+dx=1e-5
+while x<=L:
+    d2psi=-k**2*(np.sqrt(2/L))*np.sin(k*x)
+    dpsi=dpsi+d2psi*dx
+    psi=psi+dpsi*dx
+    x=x+dx
+    PSI2.append(psi+5)
+    P2.append(((np.sqrt(2/L)*np.sin(k*x))**2)+5)
     
+n=3
+L=1
+k=n*np.pi/L
+x=0
+psi=np.sqrt(2/L)*np.sin(k*x)
+dpsi=np.sqrt(2)*k*np.cos(k*x)/np.sqrt(L)
+PSI3=[0]
+P3=[psi**2]
+dx=1e-5
+while x<=L:
+    d2psi=-k**2*(np.sqrt(2/L))*np.sin(k*x)
+    dpsi=dpsi+d2psi*dx
+    psi=psi+dpsi*dx
+    x=x+dx
+    PSI3.append(psi+10)
+    P3.append(((np.sqrt(2/L)*np.sin(k*x))**2)+10)
+
+n=4
+L=1
+k=n*np.pi/L
+x=0
+psi=np.sqrt(2/L)*np.sin(k*x)
+dpsi=np.sqrt(2)*k*np.cos(k*x)/np.sqrt(L)
+PSI4=[0]
+P4=[psi**2]
+dx=1e-5
+while x<=L:
+    d2psi=-k**2*(np.sqrt(2/L))*np.sin(k*x)
+    dpsi=dpsi+d2psi*dx
+    psi=psi+dpsi*dx
+    x=x+dx
+    PSI4.append(psi+15)
+    P4.append(((np.sqrt(2/L)*np.sin(k*x))**2)+15)    
+    
+xgrid=[0,1]
+ygrid=[[0,0],[5,5],[10,10],[15,15]]
+
+plt.figure(figsize=(3,6))
+plt.title('Wave function for Psi(x,n)')
+plt.plot(X,PSI,color='blue')
+plt.plot(xgrid,ygrid[0],color='black')
+plt.plot(xgrid,ygrid[1],color='black')
+plt.plot(xgrid,ygrid[2],color='black')
+plt.plot(xgrid,ygrid[3],color='black')
+plt.plot(X,PSI2,color='blue')
+plt.plot(X,PSI3,color='blue')
+plt.plot(X,PSI4,color='blue')
+plt.xlim(0,1)
+plt.yticks([0,5,10,15],('n=1','n=2','n=3','n=4'))
+plt.xticks([0,1],('0','L'))
+plt.show()    
+    
+plt.figure(figsize=(3,6))
+plt.title('Probability curve for Psi(x,n)')
+plt.plot(X,P,color='blue')
+plt.plot(X,P2,color='blue')
+plt.plot(X,P3,color='blue')
+plt.plot(X,P4,color='blue')
+plt.plot(xgrid,ygrid[0],color='black')
+plt.plot(xgrid,ygrid[1],color='black')
+plt.plot(xgrid,ygrid[2],color='black')
+plt.plot(xgrid,ygrid[3],color='black')
+plt.xlim(0.1,1)
+plt.yticks([0,5,10,15],('n=1','n=2','n=3','n=4'))
+plt.xticks([0,1],('0','L'))
+plt.show()
+
+n=1
+k=n*np.pi/L
+def f(x, y):
+    return np.sqrt(2/L)*np.sin(k*x)*np.sqrt(2/L)*np.sin(k*y)
+
+x = np.linspace(-L, L, 30)
+y = np.linspace(-L, L, 30)
+
+X, Y = np.meshgrid(x, y)
+Z = f(X, Y)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.contour3D(X, Y, Z, 50, cmap='viridis')
+ax.set_xlabel('x')
+plt.xticks([])
+plt.yticks([])
+ax.set_zticks([])
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+import scipy.integrate as inte
+import scipy.optimize as opt
+
+def findzeros(rightvals):
+    return np.where(np.diff(np.signbit(rightvals)))[0]
+
+def shoot_ode(E,psi_init,x,L):
+    sol=inte.odeint(schrderiv,psi_init,x,args=(L,E))
+    return sol[len(sol)-1][0]
+
+def schrderiv(y,r,L,E):
+    du2=y[0]*((L*(L+1))/(r**2)-2/r-E)
+    return [y[1],du2]
+
+def normalize(output):
+    normal = max(output)
+    return output*(1/normal)
+
+def shoothydr(psi_init,h_,L):
+    x_arr_hy=np.arange(0.0001,35.0+h_,h_)
+    E_arr = np.arange(-1,0,.001)
+    rightb=[]
+    for EE in E_arr:
+        psi=inte.odeint(schrderiv,psi_init,x_arr_hy,args=(L,EE))[:,0]
+        rightb.append(psi[len(psi)-1])
+    rightb_arr=np.asarray(rightb)
+    crossings=findzeros(rightb_arr)
+    energy_1=[]
+    for cross in crossings:
+        energy_1.append(opt.newton(shoot_ode,E_arr[cross],args=(psi_init,x_arr_hy,L)))
+    psi_out=[]
+    for EN in energy_1:
+        psi_out.append(inte.odeint(schrderiv,psi_init,x_arr_hy,args=(L,EN))[:,0])
+    return x_arr_hy,np.asarray(psi_out)
+
+def HYDRO(x,N,L):
+    if (((N-L-1)==0) and (L==0)):
+        return x*np.exp(-x)
+    elif (((N-L-1)==1) and (L==0)):
+        return (np.sqrt(2)*(-x+2)*np.exp(-x/2)/4)*x
+    elif ((N-L-1)==2):
+        return (2*np.sqrt(3)*(2*x**2/9-2*x+3)*np.exp(-x/3)/27)*x
+    elif (((N-L-1)==0) and (L==1)):
+        return (np.sqrt(6)*x*np.exp(-x/2)/12)*x
+    else:
+        print("No wavefunction found")
+        
+def plot_wave(fig,title_string,x_arr,num_arr,ana_arr,axis_list):
+    plt.cla()
+    plt.clf()
+    plt.plot(x_arr,num_arr,'b.',linewidth=4,label=r"$\Psi(\hat{x})_{num}$")
+    plt.plot(x_arr,normalize(ana_arr),'r-',label=r"$\Psi(\hat{x})_{ana}$")
+    plt.ylabel(r"$\Psi(\hat{x})$",fontsize=16)
+    plt.xlabel(r"$\hat{x}$",fontsize='small')
+    plt.axis(axis_list)
+    plt.title(title_string)
+    plt.grid()
+    
+    
+psi_0=0.0
+phi_0=1.0
+psi_init=np.asarray([psi_0,phi_0])
+h_=1.0/200.0    
+    
+fig=plt.figure()
+hydro_x,hydro_num=shoothydr(psi_init,h_,0)
+hydro_x2p,hydro_num2p=shoothydr(psi_init,h_,1)
+hydro_ana1s=HYDRO(hydro_x,1,0)
+hydro_ana2s=HYDRO(hydro_x,2,0)
+hydro_ana3s=HYDRO(hydro_x,3,0)
+hydro_ana2p=HYDRO(hydro_x,2,1)
+print("Hydrogen shooting")
+plot_wave(fig,"Hydrogen Atom, 1s",hydro_x,normalize(hydro_num[0,:]),hydro_ana1s,[-0.1,30,-0.1,1.2])
+fig1=plt.figure()
+plot_wave(fig1,"Hydrogen Atom, 2s",hydro_x,normalize(hydro_num[1,:]),hydro_ana2s,[-0.1,30,-2.2,1.2])
+fig2=plt.figure()
+plot_wave(fig2,"Hydrogen Atom, 2p",hydro_x2p,normalize(hydro_num2p[0,:]),hydro_ana2p,[-0.1,30,-0.1,1.2])
+fig3=plt.figure()
+plot_wave(fig3,"Hydrogen Atom, 3s",hydro_x,normalize(hydro_num[2,:]),hydro_ana3s,[-0.1,30,-1.2,1.2])
+
+from mayavi import mlab
+
+
+x, y, z = np.ogrid[-25:25:200j, -25:25:200j, -25:25:200j]
+r=np.sqrt(np.square(x)+np.square(y)+np.square(z))
+phi=np.arctan(y/x)
+theta=np.arccos(z/r)
+s=np.square(((r**2)/81)*(1/np.sqrt(6*np.pi))*(np.e**(-r/3))*((3*np.cos(theta)**2)-1))
+
+mlab.pipeline.volume(mlab.pipeline.scalar_field(s))
+mlab.title("3d Hydrogen")
+mlab.show()
+
+x, y, z = np.ogrid[-25:25:200j, -25:25:200j, -25:25:200j]
+r=np.sqrt(np.square(x)+np.square(y)+np.square(z))
+phi=np.arctan(y/x)
+theta=np.arccos(z/r)
+s=np.square((np.sqrt(2)/81)*(1/(np.sqrt(np.pi)))*(np.e**(-r/3))*(6-r))
+
+mlab.pipeline.volume(mlab.pipeline.scalar_field(s))
+mlab.title("3p Hydrogen")
+mlab.show()
+
+x, y, z = np.ogrid[-15:15:200j, -15:15:200j, -15:15:200j]
+r=np.sqrt(np.square(x)+np.square(y)+np.square(z))
+phi=np.arctan(y/x)
+theta=np.arccos(z/r)
+s=np.square((1/81)*(1/(np.sqrt(3*np.pi)))*(np.e**(-r/3))*(27-(18*r)+(2*r**2)))
+
+mlab.pipeline.volume(mlab.pipeline.scalar_field(s))
+mlab.title("3s Hydrogen")
+mlab.show()
+
+x, y, z = np.ogrid[-10:10:200j, -10:10:200j, -10:10:200j]
+r=np.sqrt(np.square(x)+np.square(y)+np.square(z))
+phi=np.arctan(y/x)
+theta=np.arccos(z/r)
+s=np.square((1/4)*(1/np.sqrt(2*np.pi))*r*(np.e**(-r))*np.cos(theta))
+
+mlab.pipeline.volume(mlab.pipeline.scalar_field(s))
+mlab.title("2p Hydrogen")
+mlab.show()
+
+x, y, z = np.ogrid[-10:10:200j, -10:10:200j, -10:10:200j]
+r=np.sqrt(np.square(x)+np.square(y)+np.square(z))
+phi=np.arctan(y/x)
+theta=np.arccos(z/r)
+s=np.square((1/(4*np.sqrt(2*np.pi))*(2-(r))*np.e**(-r/2)))
+
+mlab.pipeline.volume(mlab.pipeline.scalar_field(s))
+mlab.title("2s Hydrogen")
+mlab.show()
+
+x, y, z = np.ogrid[-5:5:200j, -5:5:200j, -5:5:200j]
+r=np.sqrt(np.square(x)+np.square(y)+np.square(z))
+phi=np.arctan(y/x)
+theta=np.arccos(z/r)
+s=np.square((2/(np.sqrt(np.pi)))*np.e**(-r))
+
+mlab.pipeline.volume(mlab.pipeline.scalar_field(s))
+mlab.title("1s Hydrogen")
+mlab.show()
     
     
     
